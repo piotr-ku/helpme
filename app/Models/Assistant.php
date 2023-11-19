5002832e380da33e8f7b1cee0cfeb02ab40b0d40
piotr-ku <95296395+piotr-ku@users.noreply.github.com>
2023-11-19 10:36:55 +0100

    Configure endpoints

--- app/Models/Assistant.php
+++ app/Models/Assistant.php
@@ -4,8 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use App\Models\Service;
 
 class Assistant extends Model
 {
     use HasFactory;
+
+    public $with = ['services'];
+
+    /**
+     * Get the services for the assistant.
+     */
+    public function services(): HasMany
+    {
+        return $this->hasMany(Service::class);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Models\Assistant;
+use App\Models\Request as ServiceRequest;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +16,54 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// /assistants returns all the assistants nearby
+Route::get('/assistants', function (Request $request) {
+    $assistants = Assistant::all();
+    return response()->json($assistants);
+});
+
+// /assistant/{id} returns all the services for the assistant
+Route::get('/assistant/{id}', function (Request $request, $id) {
+    $assistant = Assistant::find($id);
+    return response()->json($assistant->services);
+});
+
+// /notify/{assistance_id}/{service_id} creates a new service request
+// for the service
+Route::get('/notify/{assistance_id}/{service_id}', function (Request $request,
+    $assistance_id, $service_id) {
+
+    $serviceRequest = new ServiceRequest;
+    $serviceRequest->assistance_id = $assistance_id;
+    $serviceRequest->service_id = $service_id;
+    $serviceRequest->save();
+
+    return response()->json([
+        'status' => 'ok',
+        'request' => $serviceRequest
+    ])
+});
+
+// /approve/{id} approves a service request
+Route::get('/approve/{id}', function (Request $request) {
+    $serviceRequest = ServiceRequest::find($id);
+    $serviceRequest->status = 2;
+    $serviceRequest->save();
+
+    return response()->json([
+        'status' => 'ok',
+        'request' => $serviceRequest,
+    ]);
+});
+
+// /decline/{id} declines a service request
+Route::get('/decline/{id}', function (Request $request) {
+    $serviceRequest = ServiceRequest::find($id);
+    $serviceRequest->status = 1;
+    $serviceRequest->save();
+
+    return response()->json([
+        'status' => 'ok',
+        'request' => $serviceRequest,
+    ]);
 });
